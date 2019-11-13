9f94f7578c8634881b64c598154bc6cac4583b27
Tyler Stewart <tyler.stewart1026@gmail.com>
2019-11-12 19:05:44 -0700

    added some routing, did survey form and home html, routed button to take to survey page

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,16 @@
+var friendData = require('../data/friends')
+
+
+
+module.exports = function(app) {
+
+    app.get ("/api/friends", function(req, res) {
+        res.json(friendsData);
+    });
+    
+    app.post("/api/friends", function(req, res) {
+        friendsData.push(req.body);
+          res.json(true);
+    })
+    
+    };
